d38d1f34a51b77f65fcd36329b82279246459151
chanmxim <chanmxim@gmail.com>
2026-05-26 07:56:44 -0400

    fix deletion logic in neovim bufferline

--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -26,15 +26,23 @@ return {
 		vim.keymap.set("n", "<S-h>", "<cmd>BufferLineCyclePrev<CR>", { desc = "Previous Tab" })
 		vim.keymap.set("n", "<S-l>", "<cmd>BufferLineCycleNext<CR>", { desc = "Next Tab" })
 		vim.keymap.set("n", "<leader>c", function()
+			-- Do nothing if trying to close the last buffer
 			local listed_buffers = vim.fn.getbufinfo({ buflisted = 1 })
-
 			if #listed_buffers <= 1 then
 				return
 			end
 
-			local bufnr = vim.api.nvim_get_current_buf()
-			vim.cmd("bp")
-			vim.cmd("bd " .. bufnr)
+			local buffer_id = vim.api.nvim_get_current_buf()
+
+			-- Delete current buffer
+			local success = pcall(function()
+				vim.cmd("confirm bd " .. buffer_id)
+			end)
+
+			-- Switch to the previous buffer
+			if success and vim.api.nvim_buf_is_valid(buffer_id) == false then
+				vim.cmd("bp")
+			end
 		end, { desc = "Close Tab safely" })
 	end,
 }
